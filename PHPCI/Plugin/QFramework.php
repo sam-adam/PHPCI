270a05ac3aed4c706d7be42baa7ce2f01920d348
Samuel <samuel.suhendra@gmail.com>
2016-07-18 15:33:00 +0700

    QFramework plugin

---
+++ PHPCI/Plugin/QFramework.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace PHPCI\Plugin;
+
+use PHPCI\Builder;
+use PHPCI\BuildFactory;
+use PHPCI\Model\Build;
+use PHPCI\Model\Project;
+
+/**
+ * Class QFrameworkPlugin
+ *
+ * @package PHPCI\Plugin
+ */
+class QFrameworkPlugin implements \PHPCI\Plugin
+{
+    private $phpci;
+    private $build;
+    private $options;
+
+    public function __construct(Builder $phpci, Build $build, array $options = array())
+    {
+        $this->phpci = $phpci;
+        $this->build = $build;
+        $this->options = $options;
+    }
+
+    public function execute()
+    {
+        $build = BuildFactory::getBuildById($this->options['buildId']);
+
+        if (!$build) {
+            $this->phpci->logFailure('QFramework not found');
+        }
+
+        if (!$build instanceof Build\RemoteGitBuild) {
+            $this->phpci->logFailure('');
+        }
+
+        $build->createWorkingCopy($this->phpci, $this->phpci->buildPath.DIRECTORY_SEPARATOR.'system');
+    }
+}
